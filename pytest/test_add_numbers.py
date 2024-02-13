3ed6de0cda1a115bdf86239b393243670d6c54d6
am-kaiser <63399571+am-kaiser@users.noreply.github.com>
2024-02-13 10:41:40 +0100

    modified

--- pytest/test_add_numbers.py
+++
@@ -1,8 +0,0 @@
-from model import add_numbers
-
-def test_add_two_integers():
-    assert add_numbers.add_two_integers(3,4) == 7
-
-# def test_add_two_str():
-#     """Test will fail!"""
-#     assert add_numbers.add_two_integers('3','4') == 7
