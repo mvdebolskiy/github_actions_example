53fbf2186b7330dfe9b5569598176add47d64866
am-kaiser <63399571+am-kaiser@users.noreply.github.com>
2024-02-13 10:27:01 +0100

    fixed test and formatted

--- tests/test_add_numbers.py
+++ tests/test_add_numbers.py
@@ -1,11 +1,13 @@
 import pytest
 from model import add_numbers
 
+
 def test_add_two_integers():
-    assert add_numbers.add_two_integers(3,4) == 7
+    assert add_numbers.add_two_integers(3, 4) == 7
+
 
 def test_add_two_str():
     """Test will fail!"""
-    assert add_numbers.add_two_integers('3','4') == 7
-    # with pytest.raises(TypeError, match="Only integers are allowed"):
-    #     add_numbers.add_two_integers('3', '4')
+    # assert add_numbers.add_two_integers('3','4') == 7
+    with pytest.raises(TypeError, match="Only integers are allowed"):
+        add_numbers.add_two_integers('3', '4')
