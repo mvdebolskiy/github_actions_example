1b74d20ef8db9797f04e13b5bd50ca5e37180a86
am-kaiser <63399571+am-kaiser@users.noreply.github.com>
2024-02-13 10:43:44 +0100

    fixed formatting

--- model/add_numbers.py
+++ model/add_numbers.py
@@ -1,5 +1,5 @@
 def add_two_integers(a, b):
     if isinstance(a, int) and isinstance(b, int):
-        return a+b
+        return a + b
     else:
         raise TypeError("Only integers are allowed")
