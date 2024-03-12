78994317618712adabe34fd948020a9aad5eeebf
mvdebolskiy <80036033+mvdebolskiy@users.noreply.github.com>
2024-03-12 14:43:10 +0100

    commit bad test

--- tests/test_add_numbers.py
+++ tests/test_add_numbers.py
@@ -6,9 +6,19 @@ def test_add_two_integers():
     assert add_numbers.add_two_integers(3, 4) == 7
 
 
-@pytest.mark.xfail(reason="This test MUST fail")
+#@pytest.mark.xfail(reason="This test MUST fail")
 def test_add_two_str():
     """Test will fail!"""
     assert add_numbers.add_two_integers("3", "4") == 7
     # with pytest.raises(TypeError, match="Only integers are allowed"):
     #     add_numbers.add_two_integers('3', '4')
+
+def test_against_kgo():
+    "Regression testing"
+    fl = open("./data/baseline.txt","r")
+    c = fl.read()
+    c = c.split()
+    for i in range(0,len(c)):
+        c.append(int(c[0]))
+        c.pop(0)
+    assert add_numbers.add_two_integers(c[0],c[1]) == c[2]
