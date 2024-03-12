e900530dc1bd175a817ac4fbabcf87d5daa319b0
mvdebolskiy <80036033+mvdebolskiy@users.noreply.github.com>
2024-03-12 13:55:36 +0100

    make test expected to fail

--- tests/test_add_numbers.py
+++ tests/test_add_numbers.py
@@ -5,7 +5,7 @@ from model import add_numbers
 def test_add_two_integers():
     assert add_numbers.add_two_integers(3, 4) == 7
 
-
+@pytest.mark.xfail(reason="This test MUST fail")
 def test_add_two_str():
     """Test will fail!"""
     assert add_numbers.add_two_integers("3", "4") == 7
